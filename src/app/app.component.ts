6eb506b57eceb7bd715cf73b0f5af05df8d9a17e
BeatriceG <bgueguen68@gmail.com>
2017-02-13 11:52:50 +0100

    mon premier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,17 @@
+//importer la class pour créer un composant
 import { Component } from '@angular/core';
-
+// définir le décorateur @component
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
-})
-export class AppComponent  { name = 'Angular'; }
+  template: `<h1>Hello {{name}}, how are you doing today ?No Fuss no mess !</h1>`,
+  styles:[`
+  h1{
+    color:red;
+    text-transform:uppercase;
+    
+  }
+  `]
+}) 
+//Exporter la class du composant
+export class AppComponent  { name = 'Julien'; }
+ 
