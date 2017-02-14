74a8673a53a215d14e7b8d86d399b310c1bf4c3e
Pablo2017 <dino.consultant@outlook.com>
2017-02-14 10:05:46 +0100

    Ajout d'angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+//Pour pouvoir utiliser le ngModel, il faut ajouter la class FormsModule
+
+import {FormsModule}from '@angular/forms';
+
+import { AppComponent }  from './components/mainCmp/app.component';
+import { DahboardComponent }  from './dashboard/dashboard.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  //Ajouter les class dans le tableau des imports
+  imports:      [ BrowserModule, FormsModule ],
+
+  //Ajouter les composants dans le tableau des déclarations
+
   declarations: [ AppComponent ],
+
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
